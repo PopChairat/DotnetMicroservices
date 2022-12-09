ae181dc95da78c0a6ed86874c24ba40810c7aee7
Chairat <Chairat.ko@hatari.net>
2022-12-09 16:25:06 +0700

    [Add] Catalog-API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //Seed DB is initial data after create db
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
+
