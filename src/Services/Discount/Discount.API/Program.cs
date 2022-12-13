74a44abcc7c73fe9b8f4474edc8b4c73eb21896d
Chairat <Chairat.ko@hatari.net>
2022-12-13 21:52:18 +0700

    [Add] Setup Docker

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,6 @@
+using Discount.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 namespace Discount.API
 {
     public class Program
@@ -11,19 +14,25 @@ namespace Discount.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+            });
+
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
             var app = builder.Build();
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
             }
 
             app.UseAuthorization();
-
 
             app.MapControllers();
               
